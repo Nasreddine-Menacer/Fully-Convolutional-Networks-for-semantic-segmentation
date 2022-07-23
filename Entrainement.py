9a973974200e93f2a231cff6958cd4cfd6caa180
Nasreddine Menacer <109802352+Nasreddine-Menacer@users.noreply.github.com>
2022-07-24 01:16:05 +0200

    Add files via upload

---
+++ Entrainement.py
@@ -0,0 +1,187 @@
+
+# -*- coding: latin-1 -*-
+"""
+@author: Nasreddine
+"""
+import cv2, os
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import random
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
+import keras, sys, time, warnings
+from keras.models import *
+from keras.layers import *
+import pandas as pd 
+warnings.filterwarnings("ignore")
+
+dir_data = "dataset1/"
+dir_seg = dir_data + "/annotations_prepped_train/"
+dir_img = dir_data + "/images_prepped_train/"
+sns.set_style("whitegrid", {'axes.grid' : False})
+
+ldseg = np.array(os.listdir(dir_seg))
+fnm = ldseg[0]
+seg = cv2.imread(dir_seg + fnm ) # (360, 480, 3)
+img_is = cv2.imread(dir_img + fnm )
+mi, ma = np.min(seg), np.max(seg)
+n_classes = ma - mi + 1
+
+def give_color_to_seg_img(seg,n_classes):
+    
+    if len(seg.shape)==3:
+        seg = seg[:,:,0]
+    seg_img = np.zeros( (seg.shape[0],seg.shape[1],3) ).astype('float')
+    colors = sns.color_palette("hls", n_classes)
+    
+    for c in range(n_classes):
+        segc = (seg == c)
+        seg_img[:,:,0] += (segc*( colors[c][0] ))
+        seg_img[:,:,1] += (segc*( colors[c][1] ))
+        seg_img[:,:,2] += (segc*( colors[c][2] ))
+
+    return(seg_img)
+
+input_height , input_width = 224 , 224
+output_height , output_width = 224 , 224
+
+ldseg = np.array(os.listdir(dir_seg))
+for fnm in ldseg[np.random.choice(len(ldseg),3,replace=False)]:
+    fnm = fnm.split(".")[0]
+    seg = cv2.imread(dir_seg + fnm + ".png") # (360, 480, 3)
+    img_is = cv2.imread(dir_img + fnm + ".png")
+    seg_img = give_color_to_seg_img(seg,n_classes)
+
+def getImageArr( path , width , height ):
+        img = cv2.imread(path, 1)
+        img = np.float32(cv2.resize(img, ( width , height ))) / 127.5 - 1
+        return img
+
+def getSegmentationArr( path , nClasses ,  width , height  ):
+
+    seg_labels = np.zeros((  height , width  , nClasses ))
+    img = cv2.imread(path, 1)
+    img = cv2.resize(img, ( width , height ))
+    img = img[:, : , 0]
+
+    for c in range(nClasses):
+        seg_labels[: , : , c ] = (img == c ).astype(int)
+    return seg_labels
+
+images = os.listdir(dir_img)
+images.sort()
+segmentations  = os.listdir(dir_seg)
+segmentations.sort()
+    
+X = []
+Y = []
+for im , seg in zip(images,segmentations) :
+    X.append( getImageArr(dir_img + im , input_width , input_height )  )
+    Y.append( getSegmentationArr( dir_seg + seg , n_classes , output_width , output_height )  )
+
+X, Y = np.array(X) , np.array(Y)
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID" 
+config = tf.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.95
+config.gpu_options.visible_device_list = "2" 
+set_session(tf.Session(config=config))   
+
+VGG_Weights_path = "vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5" 
+
+def FCN8( nClasses ,  input_height=224, input_width=224):
+    assert input_height%32 == 0
+    assert input_width%32 == 0
+    IMAGE_ORDERING =  "channels_last" 
+
+    img_input = Input(shape=(input_height,input_width, 3)) ## Assume 224,224,3
+    
+    ## Block 1
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1', data_format=IMAGE_ORDERING )(img_input)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2', data_format=IMAGE_ORDERING )(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool', data_format=IMAGE_ORDERING )(x)
+    f1 = x
+    
+    # Block 2
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1', data_format=IMAGE_ORDERING )(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2', data_format=IMAGE_ORDERING )(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool', data_format=IMAGE_ORDERING )(x)
+    f2 = x
+
+    # Block 3
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1', data_format=IMAGE_ORDERING )(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2', data_format=IMAGE_ORDERING )(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3', data_format=IMAGE_ORDERING )(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool', data_format=IMAGE_ORDERING )(x)
+    pool3 = x
+
+    # Block 4
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1', data_format=IMAGE_ORDERING )(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2', data_format=IMAGE_ORDERING )(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3', data_format=IMAGE_ORDERING )(x)
+    pool4 = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool', data_format=IMAGE_ORDERING )(x)## (None, 14, 14, 512) 
+
+    # Block 5
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1', data_format=IMAGE_ORDERING )(pool4)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2', data_format=IMAGE_ORDERING )(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3', data_format=IMAGE_ORDERING )(x)
+    pool5 = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool', data_format=IMAGE_ORDERING )(x)## (None, 7, 7, 512)
+
+    vgg  = Model(  img_input , pool5  )
+    vgg.load_weights(VGG_Weights_path) 
+    
+    n = 4096
+    o = ( Conv2D( n , ( 7 , 7 ) , activation='relu' , padding='same', name="conv6", data_format=IMAGE_ORDERING))(pool5)
+    conv7 = ( Conv2D( n , ( 1 , 1 ) , activation='relu' , padding='same', name="conv7", data_format=IMAGE_ORDERING))(o)
+    conv7_4 = Conv2DTranspose( nClasses , kernel_size=(4,4) ,  strides=(4,4) , use_bias=False, data_format=IMAGE_ORDERING )(conv7)
+    pool411 = ( Conv2D( nClasses , ( 1 , 1 ) , activation='relu' , padding='same', name="pool4_11", data_format=IMAGE_ORDERING))(pool4)
+    pool411_2 = (Conv2DTranspose( nClasses , kernel_size=(2,2) ,  strides=(2,2) , use_bias=False, data_format=IMAGE_ORDERING ))(pool411)
+    pool311 = ( Conv2D( nClasses , ( 1 , 1 ) , activation='relu' , padding='same', name="pool3_11", data_format=IMAGE_ORDERING))(pool3)
+        
+    o = Add(name="add")([pool411_2, pool311, conv7_4 ])
+    o = Conv2DTranspose( nClasses , kernel_size=(8,8) ,  strides=(8,8) , use_bias=False, data_format=IMAGE_ORDERING )(o)
+    o = (Activation('softmax'))(o)
+    model = Model(img_input, o)
+    return model
+
+model = FCN8(nClasses     = n_classes,  
+             input_height = 224, 
+             input_width  = 224)
+model.summary()
+
+from sklearn.utils import shuffle
+train_rate = 0.85
+index_train = np.random.choice(X.shape[0],int(X.shape[0]*train_rate),replace=False)
+index_test  = list(set(range(X.shape[0])) - set(index_train))
+                            
+X, Y = shuffle(X,Y)
+X_train, y_train = X[index_train],Y[index_train]
+X_test, y_test = X[index_test],Y[index_test]
+print(X_train.shape, y_train.shape)
+print(X_test.shape, y_test.shape)
+
+from keras import optimizers
+
+sgd = optimizers.SGD(lr=1E-2, decay=5**(-4), momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy',
+              optimizer=sgd,
+              metrics=['accuracy'])
+
+hist1 = model.fit(X_train,y_train,
+                  validation_data=(X_test,y_test),
+                  batch_size=1,epochs=10,verbose=1)
+
+for key in ['loss', 'val_loss']:
+    plt.plot(hist1.history[key],label=key)
+plt.legend()
+plt.show()
+plt.savefig('DL_Loss.png')
+
+from keras.models import load_model
+model.save('TP_Deep_learning.h5')  
+print("Model enregistré")   
+
+
+
+
